22a3150eedf1bdad3caa88d2ffb9bf10d008cae1
Satrio Wisnu <satriowisnuap@gmail.com>
2025-09-06 19:30:05 +0700

    Praktikum 1 | 03 Pengantar Bahasa Pemrograman Dart - Bagian 2

---
+++ Praktikum 3/controlFlows.dart
@@ -0,0 +1,35 @@
+void main() {
+  // String test = "Test2";
+
+  // if (test == "Test1") {
+  //   print("Test 1");
+  // } else if (test == "Test2") {
+  //   print("Test 2");
+  // } else {
+  //   print("Something else");
+  // }
+
+  // if (test == "Test2") {
+  //   print("Test 2 Again");
+  // }
+
+  // Salah Karena String bukan Boolean
+  // String test = "true";
+  // if (test) {
+  //   print("Kebenaran");
+  // }
+
+  // Contoh Yang Benar
+  String test = "true";
+
+  if (test == "true") {
+    print("Kebenaran");
+  }
+
+  // Contoh Menggunakan Boolean
+  bool test1 = true;
+
+  if (test1) {
+    print("Kebenaran");
+  }
+}
