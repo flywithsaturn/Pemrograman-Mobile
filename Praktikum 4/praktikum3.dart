9a1697354d1312af2b3cf70337f5580b6759cba3
Satrio Wisnu <satriowisnuap@gmail.com>
2025-09-17 20:35:18 +0700

    Jobsheet 4 Praktikum 3 Eksperimen Tipe Data Maps | Langkah 3

--- Praktikum 4/praktikum3.dart
+++ Praktikum 4/praktikum3.dart
@@ -1,27 +1,34 @@
 void main() {
+  // Map dengan key bertipe String
   var gifts = {
     'first': 'partridge',
     'second': 'turtledoves',
-    'fifth': '1',
+    'fifth': 'golden rings',
   };
 
-  var nobleGases = {2: 'helium', 10: 'neon', 18: '2'};
+  // Map dengan key bertipe int
+  var nobleGases = {2: 'helium', 10: 'neon', 18: 'argon'};
 
-  print(gifts);
-  print(nobleGases);
-
-  // Tambahan
+  // Map kosong bertipe String-String
   var mhs1 = Map<String, String>();
-  gifts['first'] = 'partridge';
-  gifts['second'] = 'turtledoves';
-  gifts['fifth'] = 'golden rings';
+  mhs1['nama'] = 'Satrio Wisnu Adi Pratama';
+  mhs1['nim'] = '2341720219';
 
+  // Map kosong bertipe Int-String
   var mhs2 = Map<int, String>();
-  nobleGases[2] = 'helium';
-  nobleGases[10] = 'neon';
-  nobleGases[18] = 'argon';
+  mhs2[1] = 'Satrio Wisnu Adi Pratama';
+  mhs2[2] = '2341720219';
 
-  print(gifts);
-  print(nobleGases);
+  // Menambahkan nama & NIM juga ke gifts dan nobleGases
+  gifts['nama'] = 'Satrio Wisnu Adi Pratama';
+  gifts['nim'] = '2341720219';
+
+  nobleGases[20] = 'Satrio Wisnu Adi Pratama';
+  nobleGases[21] = '2341720219';
+
+  // Cetak hasil
+  print('gifts: $gifts');
+  print('nobleGases: $nobleGases');
+  print('mhs1: $mhs1');
+  print('mhs2: $mhs2');
 }
-
