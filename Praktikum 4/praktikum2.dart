70012029ddbba1c38ebc9a27a5bc02ed2833cba6
Satrio Wisnu <satriowisnuap@gmail.com>
2025-09-17 19:52:51 +0700

    Jobsheet 4 Praktikum 3 Eksperimen Tipe Data Maps | Langkah 1 & 3

--- Praktikum 4/praktikum2.dart
+++ Praktikum 4/praktikum2.dart
@@ -1,10 +1,16 @@
 void main() {
   var names1 = <String>{};
-  Set<String> names2 = {}; // This works, too.
-  var names3 = {}; // Creates a map, not a set.
+  Set<String> names2 = {};
+
+  // Menambahkan 1 elemen dengan .add()
+  names1.add("Satrio Wisnu Adi Pratama");
+
+  // Menambahkan banyak elemen sekaligus dengan .addAll()
+  names2.addAll({"Satrio Wisnu Adi Pratama", "2341720219"});
 
   print(names1);
   print(names2);
-  print(names3);
 }
+
+
 
