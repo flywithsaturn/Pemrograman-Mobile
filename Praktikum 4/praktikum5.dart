f95065d20f70d5ac6191d89d0da891202b06501f
Satrio Wisnu <satriowisnuap@gmail.com>
2025-09-18 09:51:01 +0700

    Jobsheet 4 Praktikum 5 Eksperimen Tipe Data Records | Langkah 4

--- Praktikum 4/praktikum5.dart
+++ Praktikum 4/praktikum5.dart
@@ -1,11 +1,13 @@
 void main() {
-  var record = ('first', a: 2, b: true, 'last');
-  print(record);
+  // var record = ('first', a: 2, b: true, 'last');
+  // print(record);
+
+  // Record type annotation in a variable declaration: (Langkah 4)
+  (String, int) mahasiswa;
+  print(mahasiswa);
 }
 
-(int, int) tukar((int, int) record) {
-  var (a, b) = record;
-  return (b, a);
-}
-
-
+// (int, int) tukar((int, int) record) {
+//   var (a, b) = record;
+//   return (b, a);
+// }
