016baa143d19d12a8cb74806e7ee07ddde571b1f
Satrio Wisnu <satriowisnuap@gmail.com>
2025-09-18 10:00:27 +0700

    Jobsheet 4 Praktikum 5 Eksperimen Tipe Data Records | Langkah 5

--- Praktikum 4/praktikum5.dart
+++ Praktikum 4/praktikum5.dart
@@ -3,8 +3,16 @@ void main() {
   // print(record);
 
   // Record type annotation in a variable declaration: (Langkah 4)
-  (String, int) mahasiswa = ('Satrio Wisnu Adi Pratama', 2341720219);
-  print(mahasiswa);
+  // (String, int) mahasiswa = ('Satrio Wisnu Adi Pratama', 2341720219);
+  // print(mahasiswa);
+
+  // Langkah 5
+  var mahasiswa2 = ('first', a: 2, b: true, 'last');
+
+  print(mahasiswa2.$1); // Prints 'first'
+  print(mahasiswa2.a); // Prints 2
+  print(mahasiswa2.b); // Prints true
+  print(mahasiswa2.$2); // Prints 'last'
 }
 
 // (int, int) tukar((int, int) record) {
