9d6b7592a7de285880914a92daf875b2cabd35f4
Satrio Wisnu <satriowisnuap@gmail.com>
2025-09-18 09:17:17 +0700

    Jobsheet 4 Praktikum 4 Eksperimen Tipe Data List: Spread dan Control-flow Operators | Langkah 5

--- Praktikum 4/praktikum4.dart
+++ Praktikum 4/praktikum4.dart
@@ -1,26 +1,35 @@
 void main() {
-  var list = [1, 2, 3];
-  var list2 = [0, ...list];
-  print(list);        
-  print(list2);       
-  print(list2.length);
+  // var list = [1, 2, 3];
+  // var list2 = [0, ...list];
+  // print(list);
+  // print(list2);
+  // print(list2.length);
 
-  var list1 = [1, 2, null];  
-  print(list1);              
-  var list3 = [0, ...list1]; 
-  print(list3.length);
+  // var list1 = [1, 2, null];
+  // print(list1);
+  // var list3 = [0, ...list1];
+  // print(list3.length);
 
-  // NIM menggunakan Spread Operator
-  var nim = [2, 3, 4, 1, 7, 2, 0, 2, 1, 9]; // NIM: 2341720219
-  var listNIM = [...nim]; 
-  print("NIM: $listNIM");
+  // // NIM menggunakan Spread Operator
+  // var nim = [2, 3, 4, 1, 7, 2, 0, 2, 1, 9]; // NIM: 2341720219
+  // var listNIM = [...nim];
+  // print("NIM: $listNIM");
 
-  // Tambahan kode nav dengan collection-if
-  bool promoActive = true; 
-  var nav = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
-  print(nav);
+  // Tambahan kode nav dengan collection-if (Langkah 4)
+  // bool promoActive = true;
+  // var nav = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
+  // print(nav);
 
-  promoActive = false; 
-  var nav2 = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
+  // promoActive = false;
+  // var nav2 = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
+  // print(nav2);
+
+  String login = 'Manager';
+  var nav2 = [
+    'Home',
+    'Furniture',
+    'Plants',
+    if (login == 'Staff') 'Inventory',
+  ];
   print(nav2);
 }
