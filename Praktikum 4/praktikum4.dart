cf5a7df6e02366ea65b001e91af5906dbad4b4f5
Satrio Wisnu <satriowisnuap@gmail.com>
2025-09-18 08:59:34 +0700

    Jobsheet 4 Praktikum 4 Eksperimen Tipe Data List: Spread dan Control-flow Operators | Langkah 3

--- Praktikum 4/praktikum4.dart
+++ Praktikum 4/praktikum4.dart
@@ -1,9 +1,14 @@
 void main() {
-  var list1 = [1, 2, 3];
-  var list2 = [0, ...list1];
-  print(list1);        // [1, 2, 3]
-  print(list2);       // [0, 1, 2, 3]
-  print(list2.length); // 4
+  var list = [1, 2, 3];
+  var list2 = [0, ...list];
+  print(list);        
+  print(list2);       
+  print(list2.length);
+
+  var list1 = [1, 2, null];  
+  print(list1);              
+  var list3 = [0, ...?list1]; 
+  print(list3.length);
 }
 
 
