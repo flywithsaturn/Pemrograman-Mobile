5fb70e8e2b66ede9c3f179fc28338140b46897b3
satrio wisnu <satriowisnuap@gmail.com>
2025-11-25 19:19:23 +0700

    Implement Plan management features with PlanProvider and PlanCreatorScreen

---
+++ Praktikum 10/master_plan/lib/provider/plan_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../models/data_layer.dart';
+
+class PlanProvider extends InheritedNotifier<ValueNotifier<List<Plan>>> {
+  const PlanProvider({
+    super.key,
+    required ValueNotifier<List<Plan>> notifier,
+    required super.child,
+  }) : super(notifier: notifier);
+
+  static ValueNotifier<List<Plan>> of(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<PlanProvider>()!
+        .notifier!;
+  }
+}
